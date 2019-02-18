351b1a30ca4e81d227986a08360c1f0f4e310f95
Tarrin <tarrin.rasmussen@aggiemail.usu.edu>
2019-02-18 11:43:25 -0700

    Filled arrays

--- conv.cpp
+++ conv.cpp
@@ -1,44 +1,59 @@
 #include <iostream>
 #include <chrono>
+#include <fstream>
 
 #define Lh  1000 // impulse
-#define Lx  100000 // input
-#define REP 100 // number of repetitions
+#define Lx  1000 // input
+#define REP 1000 // number of repetitions
 
 int main() {
 
-    int Lz = Lx + 2*(Lh-1); // Length of padded input
-    int Ly = Lh + Lx - 1; // Length of output
+    int Lz = Lx + 2*(Lh-1); // length of padded input
+    int Ly = Lh + Lx - 1; // length of output
 
-    int y[Ly] = { 10 }; // Output array
-    int x[Lz] = { 10 }; // Input array
-    int h[Lh] = { 10 }; // Impulse array
+    int y[Ly] = { 0 }; // output array
+    int x[Lz] = { 0 }; // input array
+    int h[Lh] = { 0 }; // impulse array
 
     // Fill the arrays
+    for (int i = (Lh - 1); i < (Lx + Lh - 1); i++)
+        x[i] = 1;
 
-    int acc = 0;
+    for (int i = 0; i < Lh; i++)
+        h[i] = 1;
+
+    int acc = 0; // accumulator for average
 
     for (int i = 0; i < REP; i++) {
 
-        auto start = std::chrono::system_clock::now();
+        for (int i = 0; i < Ly; i++)
+            y[i] = 0;
 
-        for (int i = 0; i < Ly; i++) {
-            for (int j = 0; j < Lh; j++) {
+        auto start = std::chrono::system_clock::now(); // start timer
+
+        // Convolution
+        for (int i = 0; i < Ly; i++)
+            for (int j = 0; j < Lh; j++)
                 y[i] += h[j] * x[i+j];
-            }
-        }
 
-        auto end = std::chrono::system_clock::now();
-        auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+        auto end = std::chrono::system_clock::now(); // end timer
+        auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
 
         acc += elapsed.count();
 
     }
 
+    std::ofstream fout("output.txt");
+
+    for (int i = 0; i < Ly; i++)
+        fout << y[i] << std::endl;
+
+    fout.close();
+
     acc /= REP;
 
     std::cout << "### TIME ELAPSED ###" << std::endl;
-    std::cout << " > " << acc << " ms" << std::endl << std::endl;
+    std::cout << " > " << acc << " us" << std::endl << std::endl;
 
     return EXIT_SUCCESS;
 }
