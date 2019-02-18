0c0dab9fd52b6e48a9cc9de9704968908c88a8d6
Tarrin <tarrin.rasmussen@aggiemail.usu.edu>
2019-02-18 10:43:05 -0700

    Added repetitions

--- conv.cpp
+++ conv.cpp
@@ -1,33 +1,44 @@
 #include <iostream>
 #include <chrono>
 
-#define Lh 1000 // impulse
-#define Lx 100000 // input
+#define Lh  1000 // impulse
+#define Lx  100000 // input
+#define REP 100 // number of repetitions
 
 int main() {
 
     int Lz = Lx + 2*(Lh-1); // Length of padded input
     int Ly = Lh + Lx - 1; // Length of output
 
-    int y[Ly] = { 0 }; // Output array
-    int x[Lz] = { 0 }; // Input array
-    int h[Lh] = { 0 }; // Impulse array
+    int y[Ly] = { 10 }; // Output array
+    int x[Lz] = { 10 }; // Input array
+    int h[Lh] = { 10 }; // Impulse array
 
     // Fill the arrays
 
-    auto start = std::chrono::system_clock::now();
+    int acc = 0;
 
-    for (int i = 0; i < Ly; i++) {
-        for (int j = 0; j < Lh; j++) {
-            y[i] += h[j] * x[i+j];
+    for (int i = 0; i < REP; i++) {
+
+        auto start = std::chrono::system_clock::now();
+
+        for (int i = 0; i < Ly; i++) {
+            for (int j = 0; j < Lh; j++) {
+                y[i] += h[j] * x[i+j];
+            }
         }
+
+        auto end = std::chrono::system_clock::now();
+        auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+        acc += elapsed.count();
+
     }
 
-    auto end = std::chrono::system_clock::now();
-    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    acc /= REP;
 
     std::cout << "### TIME ELAPSED ###" << std::endl;
-    std::cout << " > " << elapsed.count() << " ms" << std::endl << std::endl;
+    std::cout << " > " << acc << " ms" << std::endl << std::endl;
 
     return EXIT_SUCCESS;
 }
